b5e3b0fb521db45e803799f07aa1590ccf6eb25b
dernestolpz <dernesto.lopez@gmail.com>
2024-09-20 18:32:16 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function getRandomElemnt(arr) {
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  }
+
+  function generateExcuse() {
+    let excuse =
+      getRandomElemnt(who) +
+      " " +
+      getRandomElemnt(action) +
+      " " +
+      getRandomElemnt(what) +
+      " " +
+      getRandomElemnt(when) +
+      ".";
+    return excuse;
+  }
+
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
